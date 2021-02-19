71f6262f544046a69ccb3ef40cb5b9fbb808842a
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-02-18 17:39:26 -0700

    Created env...

--- src/components/translate.js
+++ src/components/translate.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+const API_KEY = process.env.REACT_APP_TRANSLATE_API_KEY;
+
 const doTranslation = async (input, languageCode, cancelToken) => {
   try {
     const { data } = await axios.post(
-      "https://translation.googleapis.com/language/translate/v2?key=AIzaSyBjDN8mhP2m81QjoAJ1AKihShRnFgoCvNM",
+      `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`,
       {
         q: input,
         target: languageCode,
